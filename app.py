1a1c90bf531bbf67ac918f2483f2f3de0eb33801
anudda <choiseonghee9307@gmail.com>
2026-04-24 01:12:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 import base64
 
 # 1. 페이지 설정
-st.set_page_config(page_title="JIYEON'S 1ST B-DAY", page_icon="⚡", layout="centered")
+st.set_page_config(page_title="돌잔치 인스타 시안", page_icon="⚡", layout="centered")
 
 # [함수] 이미지 텍스트 변환
 def get_b64(path):
